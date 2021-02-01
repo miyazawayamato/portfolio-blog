326d678dbf798e4c7fd6a3107f4571d8047a6200
miyazawayamato <yamato8781@gmail.com>
2021-02-01 23:40:24 +0900

    2/1

--- 1/adminfiles/prof.php
+++ 1/adminfiles/prof.php
@@ -1,5 +1,8 @@
 <?php
 
+
+//新規登録時に入力させる
+
 require_once 'connect.php';
 
 function getProf() {
@@ -10,4 +13,44 @@ function getProf() {
     $prof = $stmt->fetch();
     
     return $prof;
+}
+
+function valiProf($blog_title,$name,$prof_text,$file) {
+    
+    if (empty($blog_title)){
+        $errors[] = 'タイトルを入力してください';
+    } else if (20 < mb_strlen($blog_title)){
+        $errors[] = '20字以内で入力してください';
+    }
+    
+    if (empty($name)){
+        $errors[] = '名前を入力してください';
+    } else if (10 < mb_strlen($name)){
+        $errors[] = '10字以内で入力してください';
+    }
+    
+    if (empty($prof_text)){
+        $errors[] = '本文を入力してください';
+    } else if (1000 < mb_strlen($prof_text)){
+        $errors[] = '1000字以内で入力してください';
+    }
+    
+    if (is_uploaded_file($file['tmp_name'])) {
+        //basename
+        $filename = basename($file['name']);
+        $file_err = $file['error'];
+        $filesize = $file['size'];
+        // //許容する拡張子
+        $array_ext = array('jpg', 'jpeg', 'png', 'gif');
+        // //送信したファイルの拡張子を取得
+        $file_ext = pathinfo($filename, PATHINFO_EXTENSION);
+        
+        if (!in_array(strtolower($file_ext), $array_ext)) {
+            $errors[] = '適切な拡張子でお願いします';
+        } else if ($filesize > 1048576 || $file_err == 2) {
+            $errors[] = 'サイズが大きすぎます';
+        }
+    }
+    
+    return $errors;
 }
